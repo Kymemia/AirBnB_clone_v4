68fe60cc3cee8c53c162a9171b14c0710e23d05c
Kymemia <kimemiachristopherr@gmail.com>
2024-06-03 18:29:32 +0300

    2-hbnb.js

---
+++ web_dynamic/static/scripts/2-hbnb.js
@@ -0,0 +1,35 @@
+#!/usr/bin/node
+// This script is executed only when DOM is loaded
+$(document).ready(function() {
+	const amenityDict = {};
+
+	$('input:checkbox').change(function() {
+		const dataId = $(this).data('id');
+		const dataName = $(this).data('name');
+
+		if ($(this).is(':checked')) {
+			amenityDict[dataId] = dataName;
+		} else {
+			delete amenityDict[dataId];
+		}
+
+		const amenityNames = Object.values(amenityDict).join(', ');
+		$('DIV.amenities h4').text(amenityNames);
+	});
+
+	function checkApiStatus() {
+		fetch('http://0.0.0.0:5001/api/v1/status/')
+		    .then(response => response.json())
+		    .then(data => {
+			    if (data.status === 'OK') {
+				    document.querySelector('#api_status').classList.add('available');
+			    } else {
+				    document.querySelector('#api_status').classList.remove('available');
+			    }
+		    })
+		    .catch(() => {
+			    document.querySelector('#api_status').classList.remove('available');
+		    });
+	}
+	checkApiStatus();
+});
